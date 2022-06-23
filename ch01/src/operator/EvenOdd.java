625afa12c761771e1fdaf4a73000a9cfbe643c4f
kiyongee2 <kiyongee2@gmail.com>
2022-06-23 10:31:44 +0900

    ch01 source

---
+++ ch01/src/operator/EvenOdd.java
@@ -0,0 +1,22 @@
+package operator;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// 숫자를 입력받아 짝수/홀수를 판별하는 프로그램
+		// 조건 연산자 활용
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 입력 : ");
+		int n = sc.nextInt();
+		
+		String result = (n % 2 != 0) ? "홀수" : "짝수";
+		
+		System.out.println(result);
+		
+		sc.close();
+	}
+}
